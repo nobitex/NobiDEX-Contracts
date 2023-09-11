6341036d56a78dab063cc90f9895f14a0135d535
toolabi <p.toolabi@yahoo.com>
2023-09-11 23:50:42 +0330

    Feature(contract): adding beforeEach section.

--- test/unit/SwapperPausability.test.ts
+++ test/unit/SwapperPausability.test.ts
@@ -1,69 +1,85 @@
-import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 // import { ethers } from 'ethers'
-import { expect } from 'chai'
-import { deployContracts, getAccounts } from '../Utils.test'
-import hre, { ethers } from 'hardhat'
+import { expect } from "chai";
+import {
+  deployContracts,
+  getAccounts,
+  deployGnosisContract,
+} from "../Utils.test";
+import hre from "hardhat";
+import { Contract, ethers } from "ethers";
 
-describe('swaper - Pausibility', function () {
-  
-//   to run the first test block swapper should have a fallback function
-//   due to the fact that we have to deposit some ether to the swapper contract to check the ether withdraw functionality
+describe("swaper - Pausibility", function () {
+  let gnosis: Contract, proxy: Contract, provider: ethers.providers.JsonRpcProvider;
 
-//   it('should pause proxy', async function () {
-//     // arrange
-//     const provider = hre.ethers.provider
-//     await provider.ready
+  beforeEach(async function () {
+    gnosis = (await deployGnosisContract()).gnosis;
+    proxy = (await deployContracts(gnosis.address)).proxy;
+    provider = hre.ethers.provider;
+    await provider.ready;
+  });
 
-//     const { gnosis, proxy, token1 } = await loadFixture(deployContracts)
-//     const { daoMember1 } = await getAccounts()
+  //   to run the first test block swapper should have a fallback function
+  //   due to the fact that we have to deposit some ether to the swapper contract to check the ether withdraw functionality
 
-//     const balance = 100
-//     await token1.transfer(proxy.address, balance)
-//     const value = ethers.utils.parseEther('1')
+  //   it('should pause proxy', async function () {
+  //     // arrange
+  //     const provider = hre.ethers.provider
+  //     await provider.ready
 
-//     await daoMember1.sendTransaction({
-//       to: proxy.address,
-//       value: value
-//     })
+  //     const { gnosis, proxy, token1 } = await loadFixture(deployContracts)
+  //     const { daoMember1 } = await getAccounts()
 
-//     const add = ethers.constants.AddressZero
+  //     const balance = 100
+  //     await token1.transfer(proxy.address, balance)
+  //     const value = ethers.utils.parseEther('1')
 
-//     // pause swapper
-//     await proxy.connect(daoMember1).pause([add, token1.address])
-//     // assert if tokens withdrawn successfully
-//     expect(await token1.balanceOf(gnosis.address)).to.be.equal(balance)
-//     expect(await ethers.provider.getBalance(gnosis.address)).to.be.equal(value)
+  //     await daoMember1.sendTransaction({
+  //       to: proxy.address,
+  //       value: value
+  //     })
 
-//     // assert if contract's paused
-//     await expect(proxy.connect(daoMember1).updateFeeRatio(5)).to.be.revertedWith('Pausable: paused')
-//   })
-  it('should unpause swapper', async function () {
+  //     const add = ethers.constants.AddressZero
+
+  //     // pause swapper
+  //     await proxy.connect(daoMember1).pause([add, token1.address])
+  //     // assert if tokens withdrawn successfully
+  //     expect(await token1.balanceOf(gnosis.address)).to.be.equal(balance)
+  //     expect(await ethers.provider.getBalance(gnosis.address)).to.be.equal(value)
+
+  //     // assert if contract's paused
+  //     await expect(proxy.connect(daoMember1).updateFeeRatio(5)).to.be.revertedWith('Pausable: paused')
+  //   })
+  it("should unpause swapper", async function () {
     // arrange
-    const { gnosis, proxy } = await loadFixture(deployContracts)
-    const { daoMember1, daoMember2, daoMember3 } = await getAccounts()
+
+    const { daoMember1 } = await getAccounts();
 
     // pause proxy
-    await proxy.connect(daoMember1).pause([])
+    await proxy.connect(daoMember1).pause([]);
     // assert if contract's paused
-    await expect(proxy.connect(daoMember1).updateFeeRatio(5)).to.be.revertedWith('Pausable: paused')
+    await expect(
+      proxy.connect(daoMember1).updateFeeRatio(5)
+    ).to.be.revertedWith("Pausable: paused");
     // create multisi transaction to unpause proxy
-    await gnosis.connect(daoMember1).unpauseSwapper(proxy.address) 
-    await expect(proxy.unpause()).to.be.revertedWith('Pausable: not paused')
+    await gnosis.connect(daoMember1).unpauseSwapper(proxy.address);
+    await expect(proxy.unpause()).to.be.revertedWith("Pausable: not paused");
     // assert if contract's unpaused
-    
-  })
-  it('should revert if caller is not admin', async function () {
-    // arrange
-    const { proxy } = await loadFixture(deployContracts)
-    const { evil } = await getAccounts()
+  });
+  it("should revert if caller is not admin", async function () {
+    const { evil } = await getAccounts();
+
     // act & assert
-    await expect(proxy.connect(evil).pause([])).to.be.revertedWith('ERROR: unauthorized caller')
-  })
-  it('should revert unpausing not paused contract', async function () {
+    await expect(proxy.connect(evil).pause([])).to.be.revertedWith(
+      "ERROR: unauthorized caller"
+    );
+  });
+  it("should revert unpausing not paused contract", async function () {
     // arrange
-    const { proxy } = await loadFixture(deployContracts)
-    const { evil } = await getAccounts()
+
+    const { evil } = await getAccounts();
     // act & assert
-    await expect(proxy.connect(evil).unpause()).to.be.revertedWith('Pausable: not paused')
-  })
-})
+    await expect(proxy.connect(evil).unpause()).to.be.revertedWith(
+      "Pausable: not paused"
+    );
+  });
+});
