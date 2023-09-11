2ebf70e34676277ad3c6780cf875424a9b11d995
toolabi <p.toolabi@yahoo.com>
2023-09-11 23:50:19 +0330

    Feature(contract): adding beforeEach section.

--- test/unit/SwapperGetter.test.ts
+++ test/unit/SwapperGetter.test.ts
@@ -1,38 +1,34 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
-import { ethers } from "ethers";
-import { deployContracts, getAccounts } from "../Utils.test";
+import { Contract, ethers } from "ethers";
+import { deployContracts, deployGnosisContract } from "../Utils.test";
 
 describe("swaper", function () {
+  let gnosis: Contract, proxy: Contract, provider: ethers.providers.JsonRpcProvider;
+
+  beforeEach(async function () {
+    gnosis = (await deployGnosisContract()).gnosis;
+    proxy = (await deployContracts(gnosis.address)).proxy;
+    provider = hre.ethers.provider;
+    await provider.ready;
+  });
   describe("getter functions Functionality", async function () {
     it("should get the chainID", async function () {
       // arrange
-
-      const { proxy } = await loadFixture(deployContracts);
-      const provider = hre.ethers.provider;
-      await provider.ready;
       const network = await provider.getNetwork();
       const chainID = network.chainId;
       expect(await proxy.getChainID()).to.equal(chainID);
     });
     it("should get the block number", async function () {
-      const { proxy } = await loadFixture(deployContracts);
-      const provider = hre.ethers.provider;
-      await provider.ready;
-      const block = await provider.getBlock('latest');
+      const block = await provider.getBlock("latest");
       const blockNumber = block.number;
       expect(await proxy.getBlockNumber()).to.equal(blockNumber);
     });
     it("should get the contract version", async function () {
-      const { proxy } = await loadFixture(deployContracts);
-
       expect(await proxy.version()).to.equal(3);
     });
     it("should get the FeeRatioDenominator", async function () {
-        const { proxy } = await loadFixture(deployContracts);
-  
-        expect(await proxy.FeeRatioDenominator()).to.equal(1000);
-      })
+      expect(await proxy.FeeRatioDenominator()).to.equal(1000);
+    });
   });
 });
