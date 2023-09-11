b4642214820b4207939e3de60b2f4f9a8cf1336a
toolabi <p.toolabi@yahoo.com>
2023-09-11 23:50:57 +0330

    Feature(contract): adding beforeEach section.

--- test/unit/SwapperRegisterBroker.test.ts
+++ test/unit/SwapperRegisterBroker.test.ts
@@ -1,29 +1,39 @@
-import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
-import { expect } from 'chai'
-import { deployContracts, getAccounts } from '../Utils.test'
+import { expect } from "chai";
+import {
+  deployContracts,
+  getAccounts,
+  deployGnosisContract,
+} from "../Utils.test";
+import { Contract } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-describe('swapper', function () {
-  describe('`registerBrokers` Functionality', async function () {
+describe("swapper", function () {
+  describe("`registerBrokers` Functionality", async function () {
+    let gnosis: Contract,
+      proxy: Contract,
+      daoMember1: SignerWithAddress,
+      daoMember5: SignerWithAddress,
+      evil: SignerWithAddress;
+
+    beforeEach(async function () {
+      gnosis = (await deployGnosisContract()).gnosis;
+      proxy = (await deployContracts(gnosis.address)).proxy;
+      daoMember1 = (await getAccounts()).daoMember1;
+      daoMember5 = (await getAccounts()).daoMember5;
+      evil = (await getAccounts()).evil;
+    });
     it("should add a new broker to the 'brokersAdresses' mapping", async function () {
-      // arrange
-      const { proxy } = await loadFixture(deployContracts)
-      const { daoMember1, daoMember5 } = await getAccounts()
-
       //check if the address is not a broker
-      expect(await proxy.brokersAddresses(daoMember5.address)).to.equal(false)
-      await proxy.connect(daoMember1).registerBrokers([daoMember5.address])
+      expect(await proxy.brokersAddresses(daoMember5.address)).to.equal(false);
+      await proxy.connect(daoMember1).registerBrokers([daoMember5.address]);
       //assert
-      expect(await proxy.brokersAddresses(daoMember5.address)).to.equal(true)
-    })
-    it('should revert if msg.sender is not an daoMember in gnosis contract', async function () {
-      const { daoMember5, evil } = await getAccounts()
-      // arrange
-      const { proxy } = await loadFixture(deployContracts)
-
+      expect(await proxy.brokersAddresses(daoMember5.address)).to.equal(true);
+    });
+    it("should revert if msg.sender is not an daoMember in gnosis contract", async function () {
       //assert
-      await expect(proxy.connect(evil).registerBrokers([daoMember5.address])).to.be.revertedWith(
-        'ERROR: unauthorized caller'
-      )
-    })
-  })
-})
+      await expect(
+        proxy.connect(evil).registerBrokers([daoMember5.address])
+      ).to.be.revertedWith("ERROR: unauthorized caller");
+    });
+  });
+});
