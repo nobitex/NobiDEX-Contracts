d7ac6d4554148a5f92b0fab1e5931a5721cb2aeb
toolabi <p.toolabi@yahoo.com>
2023-09-11 23:49:13 +0330

    Feature(contract): adding test cases for uups proxy feature .

--- test/unit/SwapperUprgradability.test.ts
+++ test/unit/SwapperUprgradability.test.ts
@@ -1,82 +1,74 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { deployContracts, getAccounts } from "../Utils.test";
-import { gnosis } from "@wagmi/chains";
+import { deployContracts, deployGnosisContract } from "../Utils.test";
+import { Contract } from "ethers";
 
 describe("Proxy - Upgradability", function () {
+  let gnosis: Contract, proxy: Contract;
+
+  beforeEach(async function () {
+    gnosis = (await deployGnosisContract()).gnosis;
+    proxy = (await deployContracts(gnosis.address)).proxy;
+  });
 
   describe("`swapper upgradability` Functionality", async function () {
     it("should upgrade successfully", async function () {
-      // arrange
-console.log("1")
-      let{proxy} = await deployContracts();
-      console.log("2")
-      const {SwapperUpgrade,gnosis} = await deployContracts();
-      console.log("3")
       const proxyAddress = proxy.address;
-      console.log("4")
-      // act & assert
-      // create transaction
 
-      await gnosis.upgradeSwapper(proxy.address, SwapperUpgrade.address)
-    //   const SwapperUpgradable = await ethers.getContractFactory("SwapperUpgrade");
-      
-      console.log("5")
-      // assert 
+      const SwapperUpgrade = await ethers.getContractFactory("SwapperUpgrade");
+      // const upgraded = await upgrades.upgradeProxy(await proxy.address, SwapperUpgrade);
+      const swapperUpgrade = await SwapperUpgrade.deploy();
+      await swapperUpgrade.deployed();
 
-      proxy = await SwapperUpgrade.attach(proxy.address);
+      await gnosis.upgradeSwapper(proxy.address, swapperUpgrade.address);
+
+      const swapperUpgraded = await ethers.getContractFactory("SwapperUpgrade");
+      proxy = await swapperUpgraded.attach(proxy.address);
+
+      // assert
+
       const newAddress = await proxy.testUpgradeability();
       expect(newAddress).to.be.equal(proxyAddress);
     });
-    // it("should revert if caller is not a dao member", async function () {
-    //   // arrange
-    //   let { proxy } = await loadFixture(
-    //     deployContracts
-    //   );
-    //   const multiSigV2 = await deployDAOMultiSigV2();
-    //   const {daoMember1,daoMember2,daoMember3,evil } = await getAccounts();
-    //   // act & assert
-    //   // create transaction
-    //   await proxy.connect(daoMember1).createUpgradeDaoMultiSigTransaction(multiSigV2.address);
-    //   const [txs,index] = await proxy.getPendingTransactions(0, daoMember1.address)
-    //   const txId = txs[index-1].id;
-    //   // approve newly created transaction and assert to fail
-    //   await proxy.connect(daoMember2).approveTransaction(txId);
-    //   await proxy.connect(daoMember3).approveTransaction(txId);
-    //   await expect(proxy.connect(evil).upgradeTo(multiSigV2.address)).to.be.revertedWith("ERROR: invalid caller");
-    // });
-    // it("should revert if multisig not used", async function () {
-    //   // arrange
-    //   let { proxy } = await loadFixture(
-    //     deployContracts
-    //   );
-    //   const {daoMember1 } = await getAccounts();
+    it("should revert if caller is not a dao member", async function () {
+      const SwapperUpgrade = await ethers.getContractFactory("SwapperUpgrade");
+      // const upgraded = await upgrades.upgradeProxy(await proxy.address, SwapperUpgrade);
+      const swapperUpgrade = await SwapperUpgrade.deploy();
+      await swapperUpgrade.deployed();
 
-    //   const multiSigV2 = await deployDAOMultiSigV2();
-    //   // act & assert
-    //   await expect(proxy.connect(daoMember1).upgradeTo(multiSigV2.address)).to.be.revertedWith("ERROR: upgrade to zero address");
-    // });
-    
-    // it("should revert if new address is not equal to preset address", async function () {
-    //   // arrange
-    //   let { proxy } = await loadFixture(
-    //     deployContracts
-    //   );
-    //   const multiSigV2_1 = await deployDAOMultiSigV2();
-    //   const multiSigV2_2= await deployDAOMultiSigV2();
+      await expect(proxy.upgradeTo(swapperUpgrade.address)).to.be.revertedWith(
+        "ERROR: unauthorized caller"
+      );
+    });
+    it("should revert if implementation address is zero", async function () {
+      const zeroAdd = ethers.constants.AddressZero;
+      const SwapperUpgrade = await ethers.getContractFactory("SwapperUpgrade");
+      // const upgraded = await upgrades.upgradeProxy(await proxy.address, SwapperUpgrade);
+      const swapperUpgrade = await SwapperUpgrade.deploy();
+      await swapperUpgrade.deployed();
 
-    //   const {daoMember1,daoMember2,daoMember3} = await getAccounts();
-    //   // act & assert
-    //   // create transaction
-    //   await proxy.connect(daoMember1).createUpgradeDaoMultiSigTransaction(multiSigV2_1.address);
-    //   const [txs,index] = await proxy.getPendingTransactions(0, daoMember1.address)
-    //   const txId = txs[index-1].id;
-    //   // approve newly created transaction
-    //   await proxy.connect(daoMember2).approveTransaction(txId);
-    //   await proxy.connect(daoMember3).approveTransaction(txId);
-    //   // assert to fail
-    //   await expect(proxy.connect(daoMember3).upgradeTo(multiSigV2_2.address)).to.be.revertedWith("ERROR: implementation address don't match with preset address");
-    // });
+      await expect(
+        gnosis.upgradeSwapper(proxy.address, zeroAdd)
+      ).to.be.revertedWith("ERROR: external call failed.");
+    });
+    it("should check the Data Migration", async function () {
+      const proxyAddress = proxy.address;
+
+      const SwapperUpgrade = await ethers.getContractFactory("SwapperUpgrade");
+      // const upgraded = await upgrades.upgradeProxy(await proxy.address, SwapperUpgrade);
+      const swapperUpgrade = await SwapperUpgrade.deploy();
+      await swapperUpgrade.deployed();
+
+      await gnosis.upgradeSwapper(proxy.address, swapperUpgrade.address);
+
+      const swapperUpgraded = await ethers.getContractFactory("SwapperUpgrade");
+      proxy = await swapperUpgraded.attach(proxy.address);
+
+      // assert
+
+      const newAddress = await proxy.testUpgradeability();
+      expect(newAddress).to.be.equal(proxyAddress);
+      expect(await proxy.maxFeeRatio()).to.be.equal(20);
+    });
   });
 });
