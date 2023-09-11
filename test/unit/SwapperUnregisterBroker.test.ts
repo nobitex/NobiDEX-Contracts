ab120ca41df773fb4569d59ee6d16e0e79964a70
toolabi <p.toolabi@yahoo.com>
2023-09-11 23:52:06 +0330

    Feature(contract): adding beforeEach section.

--- test/unit/SwapperUnregisterBroker.test.ts
+++ test/unit/SwapperUnregisterBroker.test.ts
@@ -1,29 +1,46 @@
-import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
-import { expect } from 'chai'
-import { deployContracts, getAccounts } from '../Utils.test'
+import { expect } from "chai";
+import {
+  deployContracts,
+  getAccounts,
+  deployGnosisContract,
+} from "../Utils.test";
+import { Contract, ethers } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import hre from "hardhat";
 
-describe('swapper', function () {
-  describe('`unregisterBrokers` Functionality', async function () {
-    it('should remove a broker from the mapping', async function () {
-      // arrange
-      const { proxy } = await loadFixture(deployContracts)
-      const { daoMember1, daoMember4 } = await getAccounts()
+describe("swapper", function () {
+  let gnosis: Contract,
+    daoMember1: SignerWithAddress,
+    daoMember4: SignerWithAddress,
+    evil: SignerWithAddress,
+    proxy: ethers.Contract,
+    provider: ethers.providers.JsonRpcProvider;
 
+  beforeEach(async function () {
+    gnosis = (await deployGnosisContract()).gnosis;
+    proxy = (await deployContracts(gnosis.address)).proxy;
+
+    daoMember1 = (await getAccounts()).daoMember1;
+
+    daoMember4 = (await getAccounts()).daoMember4;
+    evil = (await getAccounts()).evil;
+
+    provider = hre.ethers.provider;
+    await provider.ready;
+  });
+  describe("`unregisterBrokers` Functionality", async function () {
+    it("should remove a broker from the mapping", async function () {
       //check if the address is not a broker
-      expect(await proxy.brokersAddresses(daoMember4.address)).to.equal(true)
-      await proxy.connect(daoMember1).unregisterBrokers([daoMember4.address])
+      expect(await proxy.brokersAddresses(daoMember4.address)).to.equal(true);
+      await proxy.connect(daoMember1).unregisterBrokers([daoMember4.address]);
       //assert
-      expect(await proxy.brokersAddresses(daoMember4.address)).to.equal(false)
-    })
-    it('should revert if msg.sender is not an daoMember in Admin contract', async function () {
-      const { daoMember4, evil } = await getAccounts()
-      // arrange
-      const { proxy } = await loadFixture(deployContracts)
-
+      expect(await proxy.brokersAddresses(daoMember4.address)).to.equal(false);
+    });
+    it("should revert if msg.sender is not an daoMember in Admin contract", async function () {
       //assert
-      await expect(proxy.connect(evil).unregisterBrokers([daoMember4.address])).to.be.revertedWith(
-        'ERROR: unauthorized caller'
-      )
-    })
-  })
-})
+      await expect(
+        proxy.connect(evil).unregisterBrokers([daoMember4.address])
+      ).to.be.revertedWith("ERROR: unauthorized caller");
+    });
+  });
+});
